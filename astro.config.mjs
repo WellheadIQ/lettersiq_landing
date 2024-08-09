5f3a84e56681947a311b28e26f123df1543c39a2
paras360 <paras@paras360.com>
2024-08-09 10:09:01 -0500

    adding google tag part2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), [partytown()]],
   vite: {
     assetsInclude: ['**/*.png']
   }
